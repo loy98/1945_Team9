a549fe67500d7db350b390e743d72e88b3678396
kimmjiinn <nankimminjin@gmail.com>
2025-04-02 00:05:35 +0900

    add: MissileManager
    가상함수화

--- 1945_Team9/MissileManager.h
+++ 1945_Team9/MissileManager.h
@@ -3,24 +3,33 @@
 #include "config.h"
 class Missile;
 class MissileFactory;
+class MisisileController;
 class MissileManager : public GameObject
 {
 public:
 	MissileManager();
-	~MissileManager();
+	virtual ~MissileManager();
 
-	void Init();
-	void Release();
-	void Update();
-	void Render(HDC hdc, bool isFlip);
+	virtual void Init() = 0;
+	virtual void Release() = 0;
 
-	void Launch(FPOINT pos);
+	virtual void Update() = 0;
+	virtual void Render(HDC hdc, bool isFlip) = 0;
+	virtual void Launch(FPOINT pos) = 0;
 
-	void AddMissile(Missile* missile);
-	Missile* CreateMissile(MissileType type, FPOINT pos, float angle, float speed);
-	const int GetMissileListSize() { return MissileList.size(); }
-private:
-	vector<Missile*> MissileList;
-	vector<MissileFactory*> factoryList{ (int)(MissileType::MissileTypeLength), nullptr };
+	virtual void AddMissile() = 0;		// 팩토리에서 addMissile 불러옴.
+	//const int GetMissileListSize() { return vecMissileList.size(); }
+
+	inline void SetOwner(GameObject * owner){ this->owner = owner; }
+
+protected:
+	MissileFactory* missileFactory;
+	vector<Missile*>::iterator iter;
+	GameObject* owner;
+
+	//FPOINT pos;		// 미사일에게 넘겨줄 포지션(플레이어 포지션)
+	//float angle;	// 미사일에게 넘겨줄 각도
 };
+
+
 
