2efee4e132d7d50a467b0103d14618690f28bf85
kimmjiinn <nankimminjin@gmail.com>
2025-04-02 00:07:28 +0900

    add: LaserManager

---
+++ 1945_Team9/LaserMissileManager.cpp
@@ -0,0 +1,99 @@
+#include "LaserMissileManager.h"
+#include "MissileFactory.h"
+#include "Missile.h"
+
+LaserMissileManager::LaserMissileManager()
+{
+}
+
+LaserMissileManager::~LaserMissileManager()
+{
+}
+
+void LaserMissileManager::Init()
+{
+	vecMissileList.resize(1);
+	missileFactory = new LaserMissileFactory;
+
+	for (iter = vecMissileList.begin(); iter != vecMissileList.end(); iter++)
+	{
+		(*iter) = missileFactory->AddMissile();
+	}
+}
+
+void LaserMissileManager::Release()
+{
+	for (auto& missile : vecMissileList)
+	{
+		if (missile)
+		{
+			missile->Release();
+			delete missile;
+			missile = nullptr;
+		}
+	}
+
+	if (missileFactory)
+	{
+		delete missileFactory;
+		missileFactory = nullptr;
+	}
+
+	//for (iter = vecMissileList.begin(); iter != vecMissileList.end(); iter++)
+	//{
+	//	(*iter)->Release();
+	//}
+}
+
+void LaserMissileManager::Update()
+{
+	for (auto& missile : vecMissileList)
+	{
+		if (missile && missile->GetIsActived())
+		{
+			missile->SetPos(owner->GetPos());
+			missile->Update();
+		}
+	}
+
+	//for (iter = vecMissileList.begin(); iter != vecMissileList.end(); iter++)
+	//{
+	//	(*iter)->Update();
+	//}
+}
+
+void LaserMissileManager::Render(HDC hdc, bool isFlip)
+{
+	for (auto missile : vecMissileList)
+	{
+		if (missile)
+			missile->Render(hdc, isFlip);
+	}
+}
+
+void LaserMissileManager::Launch(FPOINT pos)
+{
+	for (auto missile : vecMissileList)
+	{
+		if (missile && !missile->GetIsActived())
+		{
+			missile->ReLoad(pos);
+			break;
+		}
+	}
+
+	/*for (iter = vecMissileList.begin(); iter != vecMissileList.end(); iter++)
+	{
+		if (!(*iter)->GetIsActived())
+		{
+			(*iter)->ReLoad(pos);
+			break;
+		}
+	}*/
+}
+
+void LaserMissileManager::AddMissile()
+{
+	vecMissileList.push_back(missileFactory->AddMissile());
+	vecMissileList.back()->SetOwner(owner);
+}
