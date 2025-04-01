bbc68460ae33caadcc162417ef128b3e5c81edd8
kimmjiinn <nankimminjin@gmail.com>
2025-04-02 00:04:29 +0900

    add: HomingMissile

--- 1945_Team9/HomingMissile.cpp
+++ 1945_Team9/HomingMissile.cpp
@@ -1,21 +1,32 @@
 #include "HomingMissile.h"
 #include "Image.h"
+#include "CommonFunction.h"
+#include "MissileController.h"
 
 void HomingMissile::Init()
 {
-	isActived;
-	moveSpeed;
-	angle;
-	destination;
+	pos = { 0, 0 };
+	isActived = false;
+	angle = 90.0f;
 
-	owner;
-	controller;
-	idle;
-	animationFrame;
-	left;
-	right;
-	currPos;
-	prevPos;
+	size = { 20,20 };
+	rc = GetRectAtCenter(pos.x, pos.y, size.x, size.y);
+
+	moveSpeed = 500.0f;
+	animationFrame = 0;
+	missileType = MissileType::Homing;
+
+	image = ImageManager::GetInstance()->AddImage(L"1945bullet", TEXT("Image\\1945bulletTest.bmp"), 33, 33, 1, 1, false, true, RGB(248, 0, 248));
+
+	controller = new NormalController();
+	
+	
+	//idle;
+	//animationFrame;
+	//left;
+	//right;
+	//currPos;
+	//prevPos;
 }
 
 void HomingMissile::Release()
@@ -29,15 +40,16 @@ void HomingMissile::Update()
 
 void HomingMissile::Render(HDC hdc, bool isFlip)
 {
+	Super::Render(hdc, isFlip);	// collision
 }
 
-void HomingMissile::ReLoad(FPOINT pos)
-{
-}
-
-void HomingMissile::Move()
-{
-}
+//void HomingMissile::ReLoad(FPOINT pos)
+//{
+//}
+//
+//void HomingMissile::Move()
+//{
+//}
 
 void HomingMissile::UpdateImage()
 {
